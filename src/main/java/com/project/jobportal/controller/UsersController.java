f07ed5a88ac31d6575bb39bf33babade92f3c498
Enes Uluc <enesuluc04@gmail.com>
2025-09-29 13:13:39 +0300

    Security Configuration is ready

--- src/main/java/com/project/jobportal/controller/UsersController.java
+++ src/main/java/com/project/jobportal/controller/UsersController.java
@@ -5,11 +5,15 @@ import com.project.jobportal.entity.Users;
 import com.project.jobportal.entity.UsersType;
 import com.project.jobportal.services.UsersService;
 import com.project.jobportal.services.UsersTypeService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -48,5 +52,18 @@ public class UsersController {
         }
         usersService.addNew(dtoUsers);
         return "dashboard";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null){
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/logout";
     }
 }
--- src/main/java/com/project/jobportal/services/UsersService.java
+++ src/main/java/com/project/jobportal/services/UsersService.java
@@ -7,7 +7,14 @@ import com.project.jobportal.entity.Users;
 import com.project.jobportal.repository.JobSeekerProfileRepository;
 import com.project.jobportal.repository.RecruiterProfileRepository;
 import com.project.jobportal.repository.UsersRepository;
+import org.antlr.v4.runtime.RecognitionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -18,19 +25,21 @@ public class UsersService {
     private final UsersRepository usersRepository;
     private final JobSeekerProfileRepository jobSeekerProfileRepository;
     private final RecruiterProfileRepository recruiterProfileRepository;
+    private final PasswordEncoder passwordEncoder;
     @Autowired
     UsersService(UsersRepository usersRepository, JobSeekerProfileRepository jobSeekerProfileRepository,
-                 RecruiterProfileRepository recruiterProfileRepository){
+                 RecruiterProfileRepository recruiterProfileRepository, PasswordEncoder passwordEncoder){
         this.usersRepository = usersRepository;
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
         this.recruiterProfileRepository = recruiterProfileRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Users addNew(DtoUsers dtoUsers){
         // Convert the user
         Users user = new Users();
         user.setEmail(dtoUsers.getEmail());
-        user.setPassword(dtoUsers.getPassword());
+        user.setPassword(passwordEncoder.encode(dtoUsers.getPassword()));
         user.setUsersType(dtoUsers.getUsersType());
 
         user.setActive(true);
@@ -38,9 +47,12 @@ public class UsersService {
 
         Users savedUser = usersRepository.save(user);
         int userTypeId = user.getUsersType().getUserTypeId();
+
+        // If this is a Recruiter, save the recruiter_profile table
         if(userTypeId == 1){
             recruiterProfileRepository.save(new RecruiterProfile(user));
         }else{
+            // save the job_seeker_profile
             jobSeekerProfileRepository.save(new JobSeekerProfile(user));
         }
         return savedUser;
@@ -50,4 +62,21 @@ public class UsersService {
         return usersRepository.findByEmail(email);
     }
 
+    public Object getCurrentUserProfile() {
+        Authentication authenticaton = SecurityContextHolder.getContext().getAuthentication();
+        if(!(authenticaton instanceof AnonymousAuthenticationToken)){
+            String username = authenticaton.getName();
+            Users user = usersRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Could not found user"));
+            int userId = user.getUserId();
+
+            if(authenticaton.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))){
+                return recruiterProfileRepository.findById(userId)
+                        .orElse(new RecruiterProfile());
+            }else{
+                return jobSeekerProfileRepository.findById(userId).orElse(new JobSeekerProfile());
+            }
+
+        }
+        return null;
+    }
 }
