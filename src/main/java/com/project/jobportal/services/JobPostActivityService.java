6895dc30c41bc76fe70b039cf181b73b552333f5
Enes Uluc <enesuluc04@gmail.com>
2025-10-05 09:34:54 +0300

    JobSeekerService is ready and compatible with JobPostActivity

--- src/main/java/com/project/jobportal/services/JobPostActivityService.java
+++ src/main/java/com/project/jobportal/services/JobPostActivityService.java
@@ -10,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class JobPostActivityService {
@@ -43,5 +45,15 @@ public class JobPostActivityService {
 
     public JobPostActivity getOne(int id) {
         return jobPostActivityRepo.findById(id).orElseThrow(() -> new RuntimeException("Could not found user"));
+    }
+
+    public List<JobPostActivity> getAll() {
+        return jobPostActivityRepo.findAll();
+    }
+
+    public List<JobPostActivity> search(String job, String location, List<String> type, List<String> remote, LocalDate searchDate) {
+        return Objects.isNull(searchDate) ? jobPostActivityRepo.searchWithoutDate(job, location, remote, type):
+                jobPostActivityRepo.search(job, location, remote, type, searchDate);
+
     }
 }
